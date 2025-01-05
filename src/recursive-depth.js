0d700a9dc6cef37afeab1bad4d8997ddb5f03196
RogerYang1996 <antontimoshenko1996@mail.ru>
2025-01-05 15:16:07 +0300

    fea: complete recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let maxDepth = 0;
+    
+    if (!Array.isArray(arr)) return 0;
+
+    arr.forEach(item => {
+        maxDepth = Math.max(maxDepth, this.calculateDepth(item));
+    });
+
+    return maxDepth + 1;
   }
 }
 
